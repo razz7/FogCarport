3f450039159ca45985389bea48f1dc3b8fcdb78e
Rasm-P <rasmus.pra@gmail.com>
2019-05-14 17:03:49 +0200

    a

--- FogCarport/src/main/java/PresentationLayer/CommandFactory.java
+++ FogCarport/src/main/java/PresentationLayer/CommandFactory.java
@@ -21,26 +21,26 @@ public class CommandFactory {
   private final Map<String, Command> commands = new HashMap();
 
     private CommandFactory() {
-        commands.put("main", new MainPageCommand("mainpage.jsp"));
-        commands.put("styklist", new StyklistPageCommand("styklist.jsp"));
-        commands.put("allmaterials", new StockMaterialsPageCommand("stockmaterialspage.jsp"));
-        commands.put("editMaterial", new EditStockMaterialPageCommand("editMaterial.jsp"));
-        commands.put("updateMaterial", new UpdateMaterialCommand("stockmaterialspage.jsp"));
-        commands.put("deleteMaterial", new DeleteStockMaterialCommand("stockmaterialspage.jsp"));
-        commands.put("createMaterial", new CreateStockMaterialCommand("stockmaterialspage.jsp"));
-        commands.put( "styklistpage", new StyklistPageCommand("styklist.jsp"));
-        commands.put("order", new OrderPageCommand("shop.jsp"));
-        commands.put("editlineitem", new EditLineItemCommand("editlineitem.jsp"));
-        commands.put("category", new categoryCommand("category.jsp"));
-        commands.put("stockListWood", new stockListWood("stockListWood.jsp"));
-        commands.put("stockListScrews", new stockListScrews("stockListScrews.jsp"));
-        commands.put("graphic", new GraphicCommand("carportSVGGraphic.jsp"));
-        commands.put("stockListTagpakke", new stockListTagpakke("stockListTagpakke.jsp"));
-        commands.put("AllOrders", new AllOrdersCommand("allOrdersPage.jsp"));
-        commands.put("updateLineitem", new UpdateLineitem("styklist.jsp"));
-        commands.put("finalPrice", new PriceCommand("PriceFinalizePage.jsp"));
-        commands.put("percent", new FinalizeOrder("allOrdersPage.sjp"));
-        commands.put("login", new LoginCommand("index.jsp"));
+        commands.put("main", new MainPageCommand("JSP/mainpage.jsp"));
+        commands.put("styklist", new StyklistPageCommand("JSP/styklist.jsp"));
+        commands.put("allmaterials", new StockMaterialsPageCommand("JSP/stockmaterialspage.jsp"));
+        commands.put("editMaterial", new EditStockMaterialPageCommand("JSP/editMaterial.jsp"));
+        commands.put("updateMaterial", new UpdateMaterialCommand("JSP/stockmaterialspage.jsp"));
+        commands.put("deleteMaterial", new DeleteStockMaterialCommand("JSP/stockmaterialspage.jsp"));
+        commands.put("createMaterial", new CreateStockMaterialCommand("JSP/stockmaterialspage.jsp"));
+        commands.put( "styklistpage", new StyklistPageCommand("JSP/styklist.jsp"));
+        commands.put("order", new OrderPageCommand("JSP/shop.jsp"));
+        commands.put("editlineitem", new EditLineItemCommand("JSP/editlineitem.jsp"));
+        commands.put("category", new categoryCommand("JSP/category.jsp"));
+        commands.put("stockListWood", new stockListWood("JSP/stockListWood.jsp"));
+        commands.put("stockListScrews", new stockListScrews("JSP/stockListScrews.jsp"));
+        commands.put("graphic", new GraphicCommand("JSP/carportSVGGraphic.jsp"));
+        commands.put("stockListTagpakke", new stockListTagpakke("JSP/stockListTagpakke.jsp"));
+        commands.put("AllOrders", new AllOrdersCommand("JSP/allOrdersPage.jsp"));
+        commands.put("updateLineitem", new UpdateLineitem("JSP/styklist.jsp"));
+        commands.put("finalPrice", new PriceCommand("JSP/PriceFinalizePage.jsp"));
+        commands.put("percent", new FinalizeOrder("JSP/allOrdersPage.sjp"));
+        commands.put("login", new LoginCommand("JSP/index.jsp"));
     }
 
     public static synchronized Command commandFrom(String key) {
