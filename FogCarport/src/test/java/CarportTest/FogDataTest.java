652356fc9445436d62bba6800d35c97ebf4ec10c
Rasm-P <rasmus.pra@gmail.com>
2019-05-20 13:03:01 +0200

    a

--- FogCarport/src/test/java/CarportTest/FogDataTest.java
+++ FogCarport/src/test/java/CarportTest/FogDataTest.java
@@ -39,13 +39,13 @@ import org.junit.Before;
 public class FogDataTest {
 
     //Test database connection.
-    private String url = "jdbc:mysql://localhost:3306/databasebasic?UseSSL=false";
-    private String user = "root";
-    private String password = "1234";
+    //private String url = "jdbc:mysql://localhost:3306/databasebasic?UseSSL=false";
+    //private String user = "root";
+    //private String password = "1234";
 
-    //private String url = "jdbc:mysql://167.99.209.155/fogTest?useUnicode=yes&characterEncoding=utf-8";
-    //private String user = "fogtest";
-    //private String password = "fogTest123!";
+    private String url = "jdbc:mysql://167.99.209.155/fogTest?useUnicode=yes&characterEncoding=utf-8";
+    private String user = "fogtest";
+    private String password = "fogTest123!";
     // Fields for adding and removing materials and orders to share between unit tests.
     private int testMaterialId;
     private int testOrderId;
